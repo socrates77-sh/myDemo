9ccbeb1e8955d68c3b44e9942812bb4da0f68220
socrates77 <zhwenrong@sina.com>
2017-06-03 21:29:24 +0800

    try @home
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- asm/make_top1.bat
+++ asm/make_top1.bat
@@ -1,7 +1,7 @@
 @echo off
 PATH=e:\tools\sn-sdcc;e:\tools\snutils\gpasm;e:\tools\snutils\gplink
 set GPASM_ARG=-p mc30p011 -I"e:\tools\sn-sdcc\share\header"
-set GPLINK_ARG=-I"e:\tools\sn-sdcc\share\lkr" -s mc30p011.lkr
+set GPLINK_ARG=-m -I"e:\tools\sn-sdcc\share\lkr" -s mc30p011.lkr
 @echo on
 
 ::note
