156432fc6350acdfbd0bf21231135a1e5a1f9070
socrates77 <zhwenrong@sina.com>
2017-06-14 23:09:36 +0800

    study @home
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- asm/init.asm
+++ asm/init.asm
@@ -1,23 +1,31 @@
 #include "0311.inc"
-
 	GLOBAL	init_timer1
 	GLOBAL	init_ports
 
-;----------------------------------------------------------------------
-;  ******************* INITIALIZE PORTS  *************************
-;----------------------------------------------------------------------
 INIT_CODE		CODE
 
-init_ports
+Read macro device, buffer
+	movar buffer
+	movra device
+	call delay
+	endm
+
+init_ports:
+	;movai 0x0d
+	Read 0x01, 0x03
 	movai 0xff
-	;iost 5
-	;iost 6
+	Read 0x01, 0x03
+	return
+	
+delay:
+	nop
+	nop
 	return
 
 ;----------------------------------------------------------------------
 ;  ******************* INITIALIZE TIMER1 MODULE  *******************
 ;----------------------------------------------------------------------
-init_timer1
+init_timer1:
 	movai b'00110000'  
 	movra MCR
 	return
