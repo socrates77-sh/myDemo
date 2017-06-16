36ad4bded3850b3ce3fb79445b001aac463baa2f
socrates77 <zhwenrong@sina.com>
2017-06-16 19:44:04 +0800

    study @office
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- asm/withmacro.asm
+++ asm/withmacro.asm
@@ -1,24 +1,18 @@
-	
 	org 0h
-	
 Read macro device, buffer
 	movar buffer
 	movra device
 	call delay
 	endm
 	
+	Read 0x01, 0x03
+	movai 01h
+	Read 0x05, 0x07
 	nop
-	
 	movai 01h
 	movra 78h
 	movai 80h
 	movra 7bh
-	
-	Read 0x01, 0x03
-	
-	movai 01h
-	
-	Read 0x05, 0x07
 	
 delay:
 	nop
