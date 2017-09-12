a9c9b4981b6612d7434304244129ed4d06f59599
socrates77 <zhwenrong@sina.com>
2017-09-12 18:19:10 +0800

    study @office
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- lib/scc.bat
+++ lib/scc.bat
@@ -7,16 +7,15 @@ PATH=%SDCC_HOME%;%SDCC_HOME%\bin;%GPUTILS_HOME%\gpasm;%GPUTILS_HOME%\gplink
 set GPASM_ARG=-p p16f87 -I"%GPUTILS_HOME%\header"
 set GPLINK_ARG=-I"%GPUTILS_HOME%\lkr" -s 16f87.lkr
 
-set SDCC_ARG=-V -S --verbose --use-non-free -mmc30 -p0311 ^
+set SDCC_ARG=-V --verbose -mmc30 -p3264 ^
 -I"%SDCC_HOME%\share\include" ^
 -Wa"-I%SDCC_HOME%\share\header" ^
--Wl"-s%SDCC_HOME%\share\lkr\mc30p011.lkr" ^
--Wl"-IE:\tools\bak\share\lib"
+--std-c99 --i-code-in-asm -c -o 
 
 @echo on
 
 ::note
-sdcc %SDCC_ARG% %1
+sdcc %SDCC_ARG% mc32p64.o mc32p64.c
 
 
 
